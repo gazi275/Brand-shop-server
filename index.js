68808405fedebbb47c44bd14c44c5f832909ae7a
gazi275 <gaziazad270@gmail.com>
2023-10-20 17:49:50 +0600

    add to cart data created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const cors = require("cors");
 const app = express();
@@ -28,11 +28,65 @@ async function run() {
   
     await client.connect();
     const productCollection = client.db("productDB").collection("product");
+    const addCartCollection = client.db("productDB").collection("addCart");
 
     app.post("/product", async (req, res) => {
         const product = req.body;
         console.log(product);
         const result = await productCollection.insertOne(product);
+        
+        res.send(result);
+      });
+
+    app.post("/details", async (req, res) => {
+        const product = req.body;
+        console.log(product);
+        const result = await addCartCollection.insertOne(product);
+        console.log(result);
+        res.send(result);
+      });
+
+      app.put("/updatedproduct/:id", async (req, res) => {
+        const id = req.params.id;
+        const data = req.body;
+        console.log("id", id, data);
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updatedProduct = {
+          $set: {
+            name: data.name,
+             price: data.price,
+             brand:data.brand,
+             image:data.image,
+             description:data.description,
+             rating:data.rating,
+             category:data.category
+          },
+          
+        };
+        console.log(updatedProduct);
+        const result = await productCollection.updateOne(
+          filter,
+          updatedProduct,
+          options
+        );
+        res.send(result);
+      });
+
+      app.get('/product/:brand', async (req, res) => {
+        const brand = req.params.brand
+        const result = await productCollection.find({brand}).toArray();
+        
+        res.send(result);
+      });
+
+      app.get("/products/:id", async (req, res) => {
+        const id = req.params.id;
+        console.log(id);
+        const query = {
+          _id: new ObjectId(id),
+        };
+        const result = await productCollection.findOne(query);
         console.log(result);
         res.send(result);
       });
